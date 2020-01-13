ee4c78c65411e19d8a8a77bece39fad34f7eb4c1
TeeKay18 <tommkar321@wp.pl>
2020-01-13 10:58:33 +0100

    Complete WWW webpage without Sass CSS

---
+++ email.js
@@ -0,0 +1,6 @@
+function get_email() {
+    var name = "tomasz.karciarz";
+    var monkey = "@";
+    var domain = "pwr.edu.pl";
+    document.getElementById("email").textContent = name + monkey + domain;
+}
